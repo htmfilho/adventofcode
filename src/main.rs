a13165548a6d5fb84c76059c723d12dfd9b4912a
Hildeberto Mendonca <me@hildeberto.com>
2023-12-01 23:37:47 -0500

    Day 1 completed

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,8 @@ pub mod utils {
 }
 
 pub mod days {
+    use std::collections::HashMap;
+
     pub fn sum_calibration_values(lines: Vec<String>) -> i32 {
         let mut sum = 0;
         for line in lines {
@@ -27,21 +29,63 @@ pub mod days {
         return sum;
     }
 
-    pub fn calibration_digits(line: String) -> (i32, i32) {
-        let mut first: i32 = -1;
-        let mut last: i32 = -1;
+    fn calibration_digits(line: String) -> (i32, i32) {
+        if line.is_empty() {
+            return (-1, -1)
+        }
 
-        for c in line.chars() {
-            if c.is_numeric() && first < 0 {
-                first = char_to_digit(c);
-                last = char_to_digit(c);
+        let mut pos_first: i32 = -1;
+        let mut str_first: String = "".to_string();
+        let mut pos_last: i32 = -1;
+        let mut str_last: String = "".to_string();
+
+        let digits = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+        let digits_number = HashMap::from([
+            (digits[0], 1), (digits[1], 2),  (digits[2], 3),  (digits[3], 4), (digits[4], 5), (digits[5], 6), (digits[6], 7), (digits[7], 8), (digits[8], 9),
+            (digits[9], 1), (digits[10], 2), (digits[11], 3), (digits[12], 4), (digits[13], 5), (digits[14], 6), (digits[15], 7), (digits[16], 8), (digits[17], 9)
+        ]);
+
+        for str_digit in digits {
+            let pos_f = match line.find(str_digit) {
+                Some(p) => p as i32,
+                None => -1
+            };
+
+            let pos_l = match line.rfind(str_digit) {
+                Some(p) => p as i32,
+                None => -1
+            };
+
+            if pos_first < 0 || pos_last < 0 {
+                pos_first = pos_f;
+                str_first = str_digit.to_string();
+                pos_last = pos_l;
+                str_last = str_digit.to_string();
                 continue;
-            } else if c.is_numeric() {
-                last = char_to_digit(c);
+            }
+
+            if pos_f >= 0 && pos_f < pos_first {
+                pos_first = pos_f;
+                str_first = str_digit.to_string();
+            } 
+            
+            if pos_l >= 0 && pos_l > pos_last {
+                pos_last = pos_l;
+                str_last = str_digit.to_string();
             }
         }
 
-        return (first, last);
+        let first = match digits_number.get(str_first.as_str()) {
+            Some(f) =>  f,
+            None => &-1
+        };
+
+        let last = match digits_number.get(str_last.as_str()) {
+            Some(l) =>  l,
+            None => &-1
+        };
+        
+        return (*first, *last);
     }
 
     fn char_to_digit(c: char) -> i32 {
