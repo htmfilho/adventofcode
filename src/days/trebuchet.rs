4b2a778a1307ef2634711be9e4b643568dcaf23c
Hildeberto Mendonca <me@hildeberto.com>
2023-12-03 13:54:06 -0500

    Added the problem statements

--- src/days/trebuchet.rs
+++ src/days/trebuchet.rs
@@ -1,5 +1,24 @@
 use std::collections::HashMap;
 
+/// Problem Statement:
+/// 
+/// The calibration document consists of lines of text; each line originally 
+/// contained a specific calibration value that the Elves now need to recover. 
+/// On each line, the calibration value can be found by combining the first 
+/// digit and the last digit (in that order) to form a single two-digit number.
+///
+/// For example:
+///
+/// 1abc2
+/// pqr3stu8vwx
+/// a1b2c3d4e5f
+/// treb7uchet
+///
+/// In this example, the calibration values of these four lines are 12, 38, 15, 
+/// and 77. Adding these together produces 142.
+/// 
+/// Consider your entire calibration document. What is the sum of all of the 
+/// calibration values?
 pub fn sum_calibration_values(lines: Vec<String>) -> i32 {
     let mut sum = 0;
     for line in lines {
